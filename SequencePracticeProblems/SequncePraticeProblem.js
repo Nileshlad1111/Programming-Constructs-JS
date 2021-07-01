92f73653e1cd43ab9e1567c46b83b3aca6fe8eec
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-07-01 12:02:15 +0530

    REFACTOR:To rename the files

--- SequencePracticeProblems/SequncePraticeProblem.js
+++ SequencePracticeProblems/SequncePraticeProblem.js
@@ -1,13 +1,13 @@
 //1.Single Digit problem
 {
     let digit = Math.floor(Math.random()*10);
-    console.log("Single Digit number "+digit);
+    console.log("Single Digit number "+ digit);
 }
 
 //2.To 1 to 6 digin number
 {
     let digit = Math.floor(Math.random()*10)%6+1;
-    console.log("Dice Roll: "+digit);
+    console.log("Dice Roll: "+ digit);
 }
 
 //3.To random digit number addition
@@ -15,7 +15,7 @@
     let dice1 = Math.floor(Math.random()*10)%6+1;
     let dice2 = Math.floor(Math.random()*10)%6+1;
     let sum = dice1+dice2;
-    console.log("Dice Roll Sum: "+sum);
+    console.log("Dice Roll Sum: "+ sum);
 }
 
 //4.To random digit number sum and average
@@ -27,7 +27,7 @@
     let digit5 = Math.floor(Math.random()*100);
     let sum = digit1+digit2+digit3+digit4+digit5;
     let avg = sum/5;
-    console.log("sum: "+ sum+ " avg: "+avg); 
+    console.log("sum: "+ sum+ " avg: "+ avg); 
 }
 
 //TO CONVERT INCH TO FEET
